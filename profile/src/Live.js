721e55983a3d4f1246a4034f92747e6468c2138e
Ankit Dubey <106301985+ankitdevlop@users.noreply.github.com>
2024-02-25 11:56:53 +0530

    chnges

--- profile/src/Live.js
+++ profile/src/Live.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import code from './Assets/0225.mp4'
-import arrow from './Assets/arro.png'
+
 
 
 
@@ -13,7 +13,12 @@ function Live() {
   <h1 className='tit'data-aos="flip-left" data-aos-easing="ease-in-out" data-aos-duration="1300">ANKIT DUBEY   </h1><br/>
   <h4 className='tit'data-aos="flip-right" data-aos-easing="ease-in-out" data-aos-duration="1300"> FULL STACK DEVELOPER </h4>
 
-<a href='#about'><img src={arrow} width={"40px"}  alt=''/></a>
+  <div id="scroll-down-animation" className='scrooll'>
+  <span class="mouse">
+    <span class="move"></span>
+  </span>
+  <h2>Scroll down</h2>
+</div>
 
 </div>
 
