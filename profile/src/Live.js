bd77556d42c8fa5b830dea4d009337146d2154a8
Ankit Dubey <106301985+ankitdevlop@users.noreply.github.com>
2024-02-25 12:08:04 +0530

    rdfs

--- profile/src/Live.js
+++ profile/src/Live.js
@@ -9,17 +9,17 @@ function Live() {
   return (
     <div className='main' id='home'>
 <video className="glow" src={code} autoPlay loop muted/>
-<div className='title' data-aos="flip-left" data-aos-easing="ease-in-out" data-aos-duration="1300">
-  <h1 className='tit'data-aos="flip-left" data-aos-easing="ease-in-out" data-aos-duration="1300">ANKIT DUBEY   </h1><br/>
-  <h4 className='tit'data-aos="flip-right" data-aos-easing="ease-in-out" data-aos-duration="1300"> FULL STACK DEVELOPER </h4>
+<div className='title' >
+  <h1 className='tit2'>ANKIT DUBEY   </h1><br/>
+  <h4 className='tit2'> FULL STACK DEVELOPER </h4>
 
+
+</div>
   <div id="scroll-down-animation" className='scrooll'>
   <span class="mouse">
     <span class="move"></span>
   </span>
   <h2>Scroll down</h2>
-</div>
-
 </div>
 
 
